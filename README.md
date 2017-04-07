2101047bc38797df0f5330aa13e48f36fe156758
Brian Cairns <brianc@evanshunt.com>
2017-04-07 17:15:07 -0600

    Fixes issue with deleted child objects being impossible to rollback to correctly
    Steps to reproduce:
    1) Create "v1" of page with "v1" child object, and Publish
    2) Delete "v1" child object, edit page to "v2", and Publish
    3) Edit parent page to "v3", and Publish
    4) Revert to the "v2" page from step 2, "v1" child will be present even though it was deleted

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Lets imagine that you have a staff profiles page where each staff member is a Da
 ## Changes in Fork
 * FIXED: If a new draft of a parent is saved first, and then a child object is edited after, and then finally the page is published, the child will have a LastEdited date later than the parent, making it fail to show up in History when viewing that parent version. (parent's LastEdited date is now updated in _versions on Publish) 
 * FIXED: If only child object(s) have changed, Publishing the parent page does not create a new record in the _versions table, making it impossible to revert back to this page state later. (if children have changed but not parent, we force a new version of parent to be created)
+* FIXED: When a versioned DataObject is "deleted" (ie from a parent's GridField), all that happens is that the DataObject's Live/Draft records are killed.  Nothing in _versions records this delete.  This makes it impossible to figure out after-the-fact (ie when attempting rollback) when the DataObject was removed from the parent. (on deletion, we record the time in a new Deleted field, and use augmentSQL() to block these for History preview/rollback)
 
 ## Usage ##
 Add the PublishWithMe extension to your page (or parent DataObject):
@@ -19,14 +20,15 @@ Still on your page (or parent DataObject) define which relations should be manag
 private static $publish_with_me = array(
     'Staff',
 );
-```	
-The DataObjects to be published with the page must have the Versioned extension, in the example above you would add this to your StaffMember object:
+```
+The DataObjects to be published with the page must have the Versioned and PublishWithMe_Child extensions, in the example above you would add this to your StaffMember object:
 ```
 private static $extensions = array(
     "Versioned('Stage', 'Live')",
+    "PublishWithMe_Child"
 );
 ```
-If your dataobjects themselves contain relations that should be published with the page then also add the PublishWithMe extension and the publish_with_me config and ensure that the child data objects have the Versioned extension etc.
+If your dataobjects themselves contain relations that should be published with the page then also add the PublishWithMe extension and the publish_with_me config and ensure that the child data objects have the Versioned + PublishWithMe_Child extensions etc.
 
 ### Installation ###
 ```
---
+++ code/PublishWithMe_Child.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * Class PublishWithMe_Child
+ *
+ * When Versioned DataObjects are deleted (ie from a $has_many relation via a GridField), all that happens is that the
+ * Live/Draft records are killed.  Nothing in _versions records this delete.  This makes it impossible to figure out
+ * after-the-fact (ie when attempting rollback) when the DataObject was removed from the parent.
+ *
+ * This Extension addresses this issue by adding a new "Deleted" field, populating it during onBeforeDelete(), and then
+ * using augmentSQL() on archive requests to insert a WHERE clause (which is exactly how Versioned works).
+ * It also clears this value (if set) after rollback on the new version
+ */
+class PublishWithMe_Child extends DataExtension
+{
+
+    private static $db = array(
+    	'Deleted' => 'SS_Datetime'
+    );
+
+	/**
+	 * Records the deletion time to the _versions table
+	 */
+	public function onBeforeDelete() {
+
+		$now = SS_Datetime::now()->Rfc2822(); // from DataObject::write()
+
+		// adapted from Versioned::publish()
+		$baseClass = ClassInfo::baseDataClass($this->owner->class);
+		$extTable = $this->owner->extendWithSuffix($baseClass);
+
+		DB::prepared_query("UPDATE \"{$extTable}_versions\"
+				SET \"Deleted\" = ?
+				WHERE \"RecordID\" = ? AND \"Version\" = ?",
+			array($now, $this->owner->ID, $this->owner->Version)
+		);
+
+	}
+
+	/**
+	 * For archive requests, limit the query to versions that have not been deleted prior to the archive date
+	 * @see Versioned::augmentSQL()
+	 * @param SQLQuery $query
+	 * @param DataQuery|null $dataQuery
+	 */
+	public function augmentSQL(SQLQuery &$query, DataQuery &$dataQuery = null) {
+
+		if (!$dataQuery || !$dataQuery->getQueryParam('Versioned.mode')) {
+			return;
+		}
+
+		if ($dataQuery->getQueryParam('Versioned.mode') == 'archive') {
+			$baseTable = ClassInfo::baseDataClass($dataQuery->dataClass());
+			$date = $dataQuery->getQueryParam('Versioned.date');
+			$query->addWhere(array(
+				"\"{$baseTable}_versions\".\"Deleted\" IS NULL OR \"{$baseTable}_versions\".\"Deleted\" > ?" => $date
+			));
+		}
+
+	}
+
+	/**
+	 * After rollback we need to clear the Deleted column of the new version
+	 * @param $version
+	 */
+	public function onAfterRollback($version) {
+
+		// at this point, a record from _versions has just been copied to Draft and _versions
+		// we need to clear the Deleted value (on both) if it is set
+		if (!empty($this->owner->Deleted)) {
+
+			$baseClass = ClassInfo::baseDataClass($this->owner->class);
+			$extTable = $this->owner->extendWithSuffix($baseClass);
+
+			DB::prepared_query("UPDATE \"{$extTable}\"
+				SET \"Deleted\" = NULL
+				WHERE \"ID\" = ?",
+				array($this->owner->ID)
+			);
+
+			DB::prepared_query("UPDATE \"{$extTable}_versions\"
+				SET \"Deleted\" = NULL
+				WHERE \"RecordID\" = ?
+				ORDER BY \"Version\" DESC
+				LIMIT 1",
+				array($this->owner->ID)
+			);
+
+			$this->owner->Deleted = null;
+
+		}
+
+	}
+
+}
