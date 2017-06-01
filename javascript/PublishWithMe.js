26f1bc4d319569f229c97a16e4136dc719b409a2
Brian Cairns <brianc@evanshunt.com>
2017-06-01 14:00:37 -0600

    Mark form as dirty using changetracker, when deleting/adding gridfield rows

--- javascript/PublishWithMe.js
+++ javascript/PublishWithMe.js
@@ -1,20 +1,26 @@
 (function($){
 
 	$.entwine('ss', function($) {
+
 		$('.ss-gridfield').entwine({
-
 			onreload: function() {
-
-				// activate "Save & Publish" button
+				// grid has been reloaded (eg, row deleted) mark form as changed
 				if( $(this).hasClass('publish-with-me') ){
-					$('#Form_EditForm').find('button[name=action_publish]').button('option', 'showingAlternate', true);
+					$('#Form_EditForm').trigger('dirty.changetracker');
 				}
-
 				// force Preview pane to reload (could not get this working via entwine)
 				$( 'iframe[name=cms-preview-iframe]' ).attr( 'src', function ( i, val ) { return val; });
-
 			}
 		});
+
+		$(".ss-gridfield.ss-gridfield-editable.publish-with-me").entwine({
+			onaddnewinline: function (e) {
+				this._super(e);
+				// inline row added, mark form as changed
+				$('#Form_EditForm').trigger('dirty.changetracker');
+			}
+		});
+
 	});
 
 }(jQuery));
